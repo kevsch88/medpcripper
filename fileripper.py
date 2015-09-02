bd949aba54885335a4af461c2170210ec7849107
metracy <matthew.etracy@gmail.com>
2015-09-01 22:23:24 -0400

    Update fileripper.py
    Added additional comments

--- fileripper.py
+++ fileripper.py
@@ -1,10 +1,11 @@
+# use pip to install these dependencies
 import csv
 import os
 import shutil
 
 
+# i is the iteration for later functions
 i = 0
-
 # This function below does the actual raw ripping of the data from the med-pc file based on the arg1 component which is the filename of the medpcfile
 def rip(arg1):
     filen = open(arg1, 'rb')
@@ -18,14 +19,13 @@ def rip(arg1):
     return ttable
 
 
-#this function below will search the current and sub-directories for any .Subject X files that you specify in the list below.
 filelist = []
 
 ######### Modify below #############
 subjects = ['Subject 92','Subject 101','Subject 106','Subject 114','Subject 117','Subject 130','Subject 132','Subject 133','Subject 141','Subject 144']
-
 ######### Modify above #############
 
+#this function below will search the current and sub-directories for any .Subject X files that you specify in the list below.
 for root, dirs, files in os.walk(".", topdown=True):
     for name in files:
         for subnum in subjects:
@@ -36,7 +36,7 @@ for root, dirs, files in os.walk(".", topdown=True):
 
 
 
-######### This is the fun part. Data from each file is put into a list of lists called 'table'. Table will look something like a excel sheet in which
+######### This is the fun part. Data from each file is put into a list of lists called 'table'. Table will look something like an excel sheet in which
 
 ######### each component of the medpc ripped sheet can be accessed with table[row][column] command. It may help to run the rip function independently in IDLE by 
 
@@ -53,13 +53,13 @@ table = []
 for filename in filelist:
     table = rip(filename)
     listt = list = [str(table[0][6]),str(table[3][2]),str(table[7][1]),str(table[103][0][1:]),str(table[95][1]),str(table[81][1]),str(table[81][2]),str(table[81][3]),str(table[81][4]),str(table[81][5]),str(table[82][1]),str(table[82][2]),str(table[82][3]),str(table[82][4]),str(table[82][5]),str(table[83][1]),str(table[83][2]),str(table[83][3]),str(table[83][4]),str(table[83][5]),str(table[84][1]),str(table[84][2]),str(table[84][3]),str(table[84][4]),str(table[84][5]),str(table[85][1]),str(table[85][2]),str(table[85][3]),str(table[85][4]),str(table[85][5]),str(table[86][1]),str(table[86][2]),str(table[86][3]),str(table[86][4]),str(table[86][5]),str(table[87][1]),str(table[87][2]),str(table[87][3]),str(table[87][4]),str(table[87][5]),str(table[88][1]),str(table[88][2]),str(table[88][3]),str(table[88][4]),str(table[88][5]),str(table[89][1]),str(table[89][2]),str(table[89][3]),str(table[89][4]),str(table[89][5]),str(table[90][1]),str(table[90][2]),str(table[90][3]),str(table[90][4]),str(table[90][5])]
-    b = open('icss.csv', 'a')
+    b = open('icss.csv', 'a') # name the csv whatever you want
     a = csv.writer(b)
     a.writerows([listt])
     b.close()
 
 
 #####Copy files to old data directory in the folder below current directory/DataOld
-destination = "../DataOld"
+destination = "../DataOld" ## I wasn't sure how to read data files in current directory without searching subdirectories within the directory, so I just set the files to copy to one directory below into the subfolder OldData
 for files in filelist:
     shutil.move(files, destination)
